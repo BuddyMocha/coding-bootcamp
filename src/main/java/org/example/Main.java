c890836960e73b513221595b3dbdac189cdccf0e
unknown <bpm1extra@gmail.com>
2024-06-24 20:23:22 -0400

    Pushing stuf to main

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,5 +3,8 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        for(int x = 0; x < 10; x++){
+            System.out.println(x);
+        }
     }
 }
