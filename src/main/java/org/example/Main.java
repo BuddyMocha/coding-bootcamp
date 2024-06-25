bb524def29600294fc15fbd4d79008df420acb7f
unknown <bpm1extra@gmail.com>
2024-06-24 20:25:54 -0400

    adding 2nd hello world

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,6 +3,7 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        System.out.println("Hello world 2");
         for(int x = 0; x < 10; x++){
             System.out.println(x);
         }
