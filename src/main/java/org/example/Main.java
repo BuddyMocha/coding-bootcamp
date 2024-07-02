41cffaefbce0e2b408a424228519cea00b1252ac
Thomas Zimmer <tzimmer31@gmail.com>
2024-07-01 20:35:20 -0400

    look ma no hands

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,8 +4,18 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         System.out.println("Hello world from Tom");
-        for(int x = 0; x < 10; x++){
+        for (int x = 0; x < 10; x++) {
             System.out.println(x);
         }
+    }
+
+    public static int countthem(String theword, String theletter) {
+        int count = 0;
+        for (int i = 0; i < theword.length(); i++) {
+            if (String.valueOf(theword.charAt(i)).equals(theletter)) {
+                count++;
+            }
+        }
+        return count;
     }
 }
